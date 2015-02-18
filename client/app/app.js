958c1cae146bbff9bcbfc4a5a54a730c4f74a61e
Dan Hems <dan.hems@telegraph.co.uk>
2015-02-18 16:06:09 +0000

    WIP: Add ACE editor via angular directive - thinking about how to imtegrate.

--- client/app/app.js
+++ client/app/app.js
@@ -5,7 +5,8 @@ angular.module('angularFullstackApp', [
   'ngResource',
   'ngSanitize',
   'ngRoute',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'ui.ace'
 ])
   .config(function ($routeProvider, $locationProvider) {
     $routeProvider
